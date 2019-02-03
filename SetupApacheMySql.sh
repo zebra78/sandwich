7ba05100755171c4956120b377f036a7946ff23d
madhu1608 <34922739+madhu1608@users.noreply.github.com>
2019-02-03 17:40:40 +0530

    aa
    aa

---
+++ SetupApacheMySql.sh
@@ -0,0 +1,15 @@
+yum update -y
+yum install -y httpd24 php56 php56-mysqlnd
+service httpd start
+chkconfig httpd on
+groupadd www
+usermod -a -G www ec2-user
+chown -R root:www /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} +
+find /var/www -type f -exec chmod 0664 {} +
+cd /var/www
+mkdir inc
+cd inc
+cp ? src/dbinfo.inc dst/dbinfo.src
+cp ? src/SamplePage.php dst/SamplePage.php
